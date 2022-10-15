01577d7b9c9063267131bf4666fb16e2d1079778
Raman Paulau <rspaulau@gmail.com>
2022-10-15 16:35:25 -0700

    sample cleanup

--- samples/Sample.Elasticsearch.Core/Controllers/HomeController.cs
+++ samples/Sample.Elasticsearch.Core/Controllers/HomeController.cs
@@ -16,10 +16,6 @@ namespace Sample.Elasticsearch.Core.Controllers {
             _client = client;
         }
 
-        public IActionResult Privacy() {
-            return View();
-        }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error() {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
